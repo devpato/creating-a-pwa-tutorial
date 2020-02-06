75843ffa972a2ca0a6d33d16fcdc97f6b80f6a91
Pato <pevargas2730@gmail.com>
2020-02-06 13:25:59 -0500

    showing desktop notification

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 "use strict";
 
+let sw = null;
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js").then(() => {
     console.log("[ServiceWorker**] - Registered");
+    displayNotification();
   });
 }
 
@@ -12,5 +15,32 @@ function toggleMenu() {
     navbar.className += " responsive";
   } else {
     navbar.className = "navbar";
+  }
+}
+
+function notification() {
+  const options = {
+    body: "Testing Our Notification",
+    icon: "/icons/icon.png"
+  };
+
+  new Notification("Hi there!", options);
+}
+
+function displayNotification() {
+  if (window.Notification && Notification.permission === "granted") {
+    notification();
+  } else if (window.Notification && Notification.permission !== "denied") {
+    Notification.requestPermission(status => {
+      if (status === "granted") {
+        notification();
+      } else {
+        alert("You denied or dismissed permissions to notifications.");
+      }
+    });
+  } else {
+    alert(
+      "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
+    );
   }
 }
