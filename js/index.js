92a19204895e4d5ac3c8db734df66184077a4563
Pato <pevargas2730@gmail.com>
2020-02-06 17:43:58 -0500

    firebase code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 "use strict";
 
 let sw = null;
+const tokenString = document.getElementById("token");
+
+const config = {
+  apiKey: "AIzaSyDx6N_Ju0rkWWpxvVST9foNJ3Pt-ab_Tu0",
+  authDomain: "pwa-push-fcm.firebaseapp.com",
+  databaseURL: "https://pwa-push-fcm.firebaseio.com",
+  projectId: "pwa-push-fcm",
+  storageBucket: "pwa-push-fcm.appspot.com",
+  messagingSenderId: "452585455460",
+  appId: "1:452585455460:web:57446702ddc55629738471"
+};
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js").then(swRegistered => {
@@ -9,6 +24,23 @@ if ("serviceWorker" in navigator) {
     displayNotification();
   });
 }
+
+messaging
+  .requestPermission()
+  .then(() => {
+    return messaging.getToken();
+  })
+  .then(token => {
+    tokenString.innerHTML = "Token Is : " + token;
+  })
+  .catch(err => {
+    console.log("Unable to get permission to notify", err);
+  });
+
+messaging.onMessage(payload => {
+  console.log("Message received. ", payload);
+  const { title, ...options } = payload.notification;
+});
 
 function toggleMenu() {
   const navbar = document.getElementById("navbar");
